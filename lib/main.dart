5bf901c996967669daaae07405ef0a93fb10c332
Raj <jainraj88@gmail.com>
2021-05-14 10:40:34 +0530

    day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_catelog_app/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
